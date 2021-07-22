2dee7a849da82b8526fdd584bc672f65e38e5f4e
MikeL Kamel <mikel1832@yahoo.com>
2021-07-22 13:38:04 -0400

    Initial Commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void HigherButton()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void LowerButton()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+
+    void StartGame()
+    {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString(); // "ToString()" allows us to convert an "int" to a "string", because ".text" only accepts strings to be displayed.
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Gets us the current scene Index integer
+
+        SceneManager.LoadScene(currentSceneIndex + 1); // Loads the next scene
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
